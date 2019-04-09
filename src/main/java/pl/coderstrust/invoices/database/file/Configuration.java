f3a5ff29a7e8172bb95357b532ea60e79a4030c1
Tomek Wrobel <wrobelt1@gmail.com>
2019-04-09 18:10:48 +0200

    Added Configuration file path class implementation

--- src/main/java/pl/coderstrust/invoices/database/file/Configuration.java
+++ src/main/java/pl/coderstrust/invoices/database/file/Configuration.java
@@ -2,4 +2,19 @@ package pl.coderstrust.invoices.database.file;
 
 public class Configuration {
 
+  private String invoicesFilePath;
+  private String invoicesIdFilePath;
+
+  public Configuration(String invoicesFilePath, String invoicesIdFilePath) {
+    this.invoicesFilePath = invoicesFilePath;
+    this.invoicesIdFilePath = invoicesIdFilePath;
+  }
+
+  public String getInvoicesFilePath() {
+    return invoicesFilePath;
+  }
+
+  public String getInvoicesIdFilePath() {
+    return invoicesIdFilePath;
+  }
 }
