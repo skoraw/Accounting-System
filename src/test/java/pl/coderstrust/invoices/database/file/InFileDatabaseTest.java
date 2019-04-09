d3facb0dcd4237ef4b1ee1917f260030e537cbac
Tomek Wrobel <wrobelt1@gmail.com>
2019-04-09 18:45:26 +0200

    Changes in InFileDatabaseTest class

--- src/test/java/pl/coderstrust/invoices/database/file/InFileDatabaseTest.java
+++ src/test/java/pl/coderstrust/invoices/database/file/InFileDatabaseTest.java
@@ -1,6 +1,7 @@
 package pl.coderstrust.invoices.database.file;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.io.File;
