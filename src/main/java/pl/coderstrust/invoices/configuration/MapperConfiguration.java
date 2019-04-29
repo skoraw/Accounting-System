3cd41480ebb02ee2af2c7d1ffc70a7b013bfcb1d
skoraw <45431110+skoraw@users.noreply.github.com>
2019-04-29 16:22:52 +0200

    Project #3 REST controller - mapper Configuration

--- src/main/java/pl/coderstrust/invoices/configuration/MapperConfiguration.java
+++ src/main/java/pl/coderstrust/invoices/configuration/MapperConfiguration.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,7 +18,7 @@ public class MapperConfiguration {
     mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
     mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
-
+    mapper.registerModule(new JSR310Module());
     return mapper;
   }
 }
