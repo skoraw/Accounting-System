c516c67fd4b7e3c9ffc7580a4a22775cd4c0cc0e
Łukasz Rosół <lukaszrosol@gmail.com>
2019-05-18 14:15:29 +0200

    Fix REST API

--- src/main/java/pl/coderstrust/invoices/controller/InvoiceController.java
+++ src/main/java/pl/coderstrust/invoices/controller/InvoiceController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.coderstrust.invoices.database.InvoiceBookException;
@@ -19,7 +18,6 @@ import pl.coderstrust.invoices.model.Invoice;
 import pl.coderstrust.invoices.service.InvoiceBook;
 
 @RestController
-@RequestMapping("/invoice")
 @Api(tags = "Invoices", description = "Available operations")
 public class InvoiceController {
 
@@ -29,19 +27,29 @@ public class InvoiceController {
     this.invoiceBook = invoiceBook;
   }
 
-  @GetMapping
-  @ApiOperation(value = "Get all invoices")
-  public Collection<Invoice> getAllInvoices() throws InvoiceBookException {
-    return invoiceBook.getAllInvoices();
+  @GetMapping("/invoices")
+  @ApiOperation(value = "Get invoices from given range of time, if dates are not provided all invoices will be returned", notes = "Date format: YYYY-MM-DD")
+  public Collection<Invoice> getInvoiceBetweenDates(
+      @RequestParam(value = "fromDate", required = false) @ApiParam(value = "Begin date - exclusive", example = "YYYY-MM-DD") String fromDate,
+      @RequestParam(value = "toDate", required = false) @ApiParam(value = "End date - exclusive", example = "YYYY-MM-DD") String toDate)
+      throws InvoiceBookException {
+    if (fromDate == null && toDate == null) {
+      return invoiceBook.getAllInvoices();
+    }
+
+    LocalDate fromDateConverter = fromDate != null ? LocalDate.from(DateTimeFormatter.ISO_LOCAL_DATE.parse(fromDate)) : LocalDate.MIN;
+    LocalDate toDateConverter   = toDate   != null ? LocalDate.from(DateTimeFormatter.ISO_LOCAL_DATE.parse(toDate))   : LocalDate.MAX;
+
+    return invoiceBook.getInvoicesBetweenDates(fromDateConverter, toDateConverter);
   }
 
-  @PutMapping
+  @PutMapping("/invoice")
   @ApiOperation(value = "Insert or update an invoice")
   public Invoice addInvoice(@ApiParam(value = "Invoice document", name = "Invoice") @RequestBody Invoice invoice) throws InvoiceBookException {
     return invoiceBook.saveInvoice(invoice);
   }
 
-  @DeleteMapping("/{id}")
+  @DeleteMapping("/invoice/{id}")
   @ApiOperation(value = "Remove the invoice")
   public Invoice removeInvoice(
       @PathVariable("id") @ApiParam(value = "Invoice ID", example = "2") Long id)
@@ -49,7 +57,7 @@ public class InvoiceController {
     return invoiceBook.removeInvoice(id);
   }
 
-  @GetMapping("/{id}")
+  @GetMapping("/invoice/{id}")
   @ApiOperation(value = "Get the invoice")
   public Invoice getInvoice(
       @PathVariable("id") @ApiParam(value = "Invoice ID", example = "2") Long id)
@@ -57,14 +65,4 @@ public class InvoiceController {
     return invoiceBook.getInvoice(id);
   }
 
-  @GetMapping("/byDates")
-  @ApiOperation(value = "Get invoices from given range of time", notes = "Date format: YYYY-MM-DD")
-  public Collection<Invoice> getInvoiceBetweenDates(
-      @RequestParam("fromDate") @ApiParam(value = "Begin date - exclusive", example = "YYYY-MM-DD") String fromDate,
-      @RequestParam("toDate") @ApiParam(value = "End date - exclusive", example = "YYYY-MM-DD") String toDate)
-      throws InvoiceBookException {
-    LocalDate fromDateConverter = LocalDate.from(DateTimeFormatter.ISO_LOCAL_DATE.parse(fromDate));
-    LocalDate toDateConverter = LocalDate.from(DateTimeFormatter.ISO_LOCAL_DATE.parse(toDate));
-    return invoiceBook.getInvoicesBetweenDates(fromDateConverter, toDateConverter);
-  }
 }
