f23f87c0e84d609244eb434c5b45c30e6c8a955f
Tomek Wrobel <wrobelt1@gmail.com>
2019-05-23 12:48:59 +0200

    Basic changes

--- src/main/java/pl/coderstrust/invoices/controller/SoapEndpoint.java
+++ src/main/java/pl/coderstrust/invoices/controller/SoapEndpoint.java
@@ -1,31 +1,30 @@
-package pl.coderstrust.invoices.service;
+package pl.coderstrust.invoices.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
-import pl.coderstrust.invoices.database.soap.SoapInvoiceRepository;
 import pl.coderstrust.invoices.model.soap.GetInvoiceRequest;
 import pl.coderstrust.invoices.model.soap.GetInvoiceResponse;
+import pl.coderstrust.invoices.service.InvoiceBook;
 
 @Endpoint
 public class SoapEndpoint {
 
   private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
 
-  private SoapInvoiceRepository soapInvoiceRepository;
+  private InvoiceBook invoiceBook;
 
   @Autowired
-  public SoapEndpoint(SoapInvoiceRepository soapInvoiceRepository) {
-    this.soapInvoiceRepository = soapInvoiceRepository;
+  public SoapEndpoint(InvoiceBook invoiceBook) {
+    this.invoiceBook = invoiceBook;
   }
 
   @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getInvoiceRequest")
   @ResponsePayload
   public GetInvoiceResponse getCountry(@RequestPayload GetInvoiceRequest request) {
     GetInvoiceResponse response = new GetInvoiceResponse();
-    //response.setCountry(soapInvoiceRepository.findCountry(request.getName()));
 
     return response;
   }
